5fe67b9ece1eb08e0e11bba7e2010dd8dab87cd3
moranneil <neilmoran0@gmail.com>
2021-01-05 09:43:31 +0000

    dbconfigTemplate.py with details on how to connect to DB 'garage_g00376338'

--- Project_Files/dbconfigTemplate.py
+++ Project_Files/dbconfigTemplate.py
@@ -1,7 +1,9 @@
+# The DB to connect to is garage_g00376338, this DB has two tables vehicle and manufacturer
+# The auth_plugin needed to be specified as mysql_native_password
 mysql={
     'host':'localhost',
     'username':'root',
     'password':'root',
     'auth_plugin':'mysql_native_password',
-    'database':'garage'
+    'database':'garage_g00376338'
 }
